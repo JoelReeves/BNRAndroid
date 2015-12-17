07fefe60168cd968f93b3e35260d0ffb82bdc01c
Joel Reeves <jreeves@tonicdesign.com>
2015-12-17 10:04:09 -0500

    defining variables for buttons in xml file

--- GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/QuizActivity.java
@@ -1,15 +1,24 @@
 package com.bromancelabs.geoquiz.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.bromancelabs.geoquiz.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class QuizActivity extends AppCompatActivity {
+
+    @Bind(R.id.btn_true) Button mTrueButton;
+    @Bind(R.id.btn_false) Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        ButterKnife.bind(this);
     }
 }
