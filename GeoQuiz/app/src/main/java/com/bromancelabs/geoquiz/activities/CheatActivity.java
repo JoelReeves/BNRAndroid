d9fcd6fb253aaf020798be5980c55a492c0916a4
Joel Reeves <jreeves@tonicdesign.com>
2015-12-22 22:05:20 -0500

    adding Intent method for CheatActivity

--- GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/CheatActivity.java
@@ -1,11 +1,20 @@
 package com.bromancelabs.geoquiz.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.bromancelabs.geoquiz.R;
 
 public class CheatActivity extends AppCompatActivity {
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bromancelabs.geoquiz.activities.answer_is_true";
+
+    public static Intent newIntent(Context context, boolean answerIsTrue) {
+        Intent intent = new Intent(context, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
