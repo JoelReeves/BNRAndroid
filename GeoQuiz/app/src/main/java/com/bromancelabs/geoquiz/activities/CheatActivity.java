8e3936d5de0d3d80b744d6de4d3b759b311e71cb
Joel Reeves <jreeves@tonicdesign.com>
2015-12-22 21:33:19 -0500

    adding CheatActivity

---
+++ GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/CheatActivity.java
@@ -0,0 +1,15 @@
+package com.bromancelabs.geoquiz.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bromancelabs.geoquiz.R;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
