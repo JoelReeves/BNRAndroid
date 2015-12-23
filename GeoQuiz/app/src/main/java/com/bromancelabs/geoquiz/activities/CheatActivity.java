93ff1e2f92d137adb2c68b5fdaaf0f0934d354f5
Joel Reeves <jreeves@tonicdesign.com>
2015-12-22 22:08:20 -0500

    receiving extra from Intent

--- GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/CheatActivity.java
@@ -10,6 +10,8 @@ import com.bromancelabs.geoquiz.R;
 public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_IS_TRUE = "com.bromancelabs.geoquiz.activities.answer_is_true";
 
+    private boolean mAnswerIsTrue;
+
     public static Intent newIntent(Context context, boolean answerIsTrue) {
         Intent intent = new Intent(context, CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
@@ -20,5 +22,7 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
     }
 }
