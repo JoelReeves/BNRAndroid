e03570162ba270d35bcd189dc601e5dfdcd5b8bc
Joel Reeves <jreeves@tonicdesign.com>
2015-12-22 19:49:21 -0500

    overriding lifecycle methods

--- GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/activities/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bromancelabs.geoquiz.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = QuizActivity.class.getSimpleName();
 
     @Bind(R.id.btn_true) Button mTrueButton;
     @Bind(R.id.btn_false) Button mFalseButton;
@@ -35,9 +37,41 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        Log.d(TAG, "onCreate(Bundle) called");
+
         ButterKnife.bind(this);
 
         updateQuestion();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @OnClick(R.id.btn_next)
