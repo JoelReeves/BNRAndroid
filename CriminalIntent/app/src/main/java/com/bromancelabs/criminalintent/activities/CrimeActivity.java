767f26847cb693995739ee385d957befaecc7f44
Joel Reeves <jreeves@tonicdesign.com>
2015-12-26 09:44:14 -0500

    creating Crime model class

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeActivity.java
@@ -1,7 +1,7 @@
 package com.bromancelabs.criminalintent.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.bromancelabs.criminalintent.R;
 
