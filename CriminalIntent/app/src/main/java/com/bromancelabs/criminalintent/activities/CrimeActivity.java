6106a40fc52e51061af5aaa4e974d951c6dbacdb
Joel Reeves <jreeves@tonicdesign.com>
2015-12-26 09:29:50 -0500

    initial setup

---
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.bromancelabs.criminalintent.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bromancelabs.criminalintent.R;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
