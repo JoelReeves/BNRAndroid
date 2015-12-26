b7bfa3fba9862b865ac080fca8f43654fbca75e6
Joel Reeves <jreeves@tonicdesign.com>
2015-12-26 11:15:00 -0500

    setting the crime's solved property based on the state of the checkbox

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.bromancelabs.criminalintent.R;
@@ -63,6 +64,13 @@ public class CrimeFragment extends Fragment {
 
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
+
+        mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
     }
 
     @Override
