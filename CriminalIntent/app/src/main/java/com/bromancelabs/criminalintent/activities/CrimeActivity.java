251c23f497fc75ff0893e892028d3fe4a0b89f58
Joel Reeves <jreeves@tonicdesign.com>
2015-12-26 10:37:35 -0500

    using fragment transactions to add CrimeFragment to CrimeActivity

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeActivity.java
@@ -1,9 +1,12 @@
 package com.bromancelabs.criminalintent.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.bromancelabs.criminalintent.R;
+import com.bromancelabs.criminalintent.fragments.CrimeFragment;
 
 public class CrimeActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit(); }
     }
 }
