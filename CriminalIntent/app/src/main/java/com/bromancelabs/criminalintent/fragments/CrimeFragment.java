83ac5b049e3120cad7db1b67100be2ac8c6d1d8c
Joel Reeves <jreeves@tonicdesign.com>
2015-12-26 10:23:34 -0500

    inflating fragment's view

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeFragment.java
@@ -1,6 +1,29 @@
 package com.bromancelabs.criminalintent.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bromancelabs.criminalintent.R;
+import com.bromancelabs.criminalintent.models.Crime;
 
 public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_crime, container, false);
+        return rootView;
+    }
 }
