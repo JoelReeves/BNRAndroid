e79c4567ab463f4498187ea05c23179e07792d91
Joel Reeves <jreeves@tonicdesign.com>
2015-12-27 16:44:37 -0500

    inflating fragment's view and setting up RecyclerView's layout manager

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
@@ -1,7 +1,40 @@
 package com.bromancelabs.criminalintent.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bromancelabs.criminalintent.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CrimeListFragment extends Fragment {
+    @Bind(R.id.crime_recycler_view) RecyclerView mCrimeRecyclerView;
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
