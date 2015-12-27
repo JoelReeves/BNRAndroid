50302f3206ce0f708363e9e1f9e25da7439d0fcc
Joel Reeves <jreeves@tonicdesign.com>
2015-12-27 16:53:58 -0500

    creating adapter

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.bromancelabs.criminalintent.R;
+import com.bromancelabs.criminalintent.models.Crime;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -39,13 +42,39 @@ public class CrimeListFragment extends Fragment {
         ButterKnife.unbind(this);
     }
 
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CrimeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.mTitleTextView.setText(crime.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
     private class CrimeHolder extends RecyclerView.ViewHolder {
         public TextView mTitleTextView;
 
         public CrimeHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
-            
+
             mTitleTextView = (TextView) itemView;
         }
     }
