94e10a7ce9b26799d9f54b3f5cbf49d40d68784c
Joel Reeves <jreeves@tonicdesign.com>
2015-12-27 17:31:55 -0500

    adding snacker methods that accept String arguments

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/utils/SnackBarUtils.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/utils/SnackBarUtils.java
@@ -13,11 +13,26 @@ public final class SnackBarUtils {
         return Snackbar.make(activity.findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT);
     }
 
+    private static Snackbar createSnackBar(@NonNull Activity activity, String text) {
+        return Snackbar.make(activity.findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT);
+    }
+
     public static void showPlainSnackBar(@NonNull Activity activity, @StringRes int text) {
         createSnackBar(activity, text).show();
     }
 
+    public static void showPlainSnackBar(@NonNull Activity activity, String text) {
+        createSnackBar(activity, text).show();
+    }
+
     public static void showStyledBar(@NonNull Activity activity, @StringRes int text, @ColorRes int textColor, @ColorRes int backgroundColor) {
+        Snackbar snackbar = createSnackBar(activity, text);
+        snackbar.setActionTextColor(ContextCompat.getColor(activity, textColor));
+        snackbar.getView().setBackgroundColor(ContextCompat.getColor(activity, backgroundColor));
+        snackbar.show();
+    }
+
+    public static void showStyledBar(@NonNull Activity activity, String text, @ColorRes int textColor, @ColorRes int backgroundColor) {
         Snackbar snackbar = createSnackBar(activity, text);
         snackbar.setActionTextColor(ContextCompat.getColor(activity, textColor));
         snackbar.getView().setBackgroundColor(ContextCompat.getColor(activity, backgroundColor));
