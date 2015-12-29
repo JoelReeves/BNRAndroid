71255e4a8e168db5b34bb94fa72ea7f0653f7203
Joel Reeves <jreeves@tonicdesign.com>
2015-12-29 13:59:06 -0500

    setting correct initial viewpager item

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimePagerActivity.java
@@ -55,5 +55,17 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+
+        setViewPagerItem(crimeId);
+
+    }
+
+    private void setViewPagerItem(UUID crimeId) {
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
     }
 }
