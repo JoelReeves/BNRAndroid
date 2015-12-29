a0f7b18183aefd8c5d4c827c999db65bb037fc32
Joel Reeves <jreeves@tonicdesign.com>
2015-12-29 13:33:32 -0500

    creating class and layout for a viewpager

---
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimePagerActivity.java
@@ -0,0 +1,15 @@
+package com.bromancelabs.criminalintent.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bromancelabs.criminalintent.R;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
