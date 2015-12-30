73c7530803cc795f230f31285f681d8f3fa9bffb
Joel Reeves <jreeves@tonicdesign.com>
2015-12-30 12:19:12 -0500

    setting the datepicker to the passed in date

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/dialogs/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/dialogs/DatePickerFragment.java
@@ -11,13 +11,17 @@ import android.widget.DatePicker;
 
 import com.bromancelabs.criminalintent.R;
 
+import java.util.Calendar;
 import java.util.Date;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DatePickerFragment extends DialogFragment {
 
-    private static final String ARG_DATE = "date";
+    @Bind(R.id.dp_dialog_date) DatePicker mDatePicker;
 
-    private DatePicker mDatePicker;
+    private static final String ARG_DATE = "date";
 
     public static DatePickerFragment newInstance(Date date) {
         Bundle args = new Bundle();
@@ -30,7 +34,19 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        ButterKnife.bind(this, v);
+
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
