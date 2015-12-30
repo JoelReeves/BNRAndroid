f7ed42373cd595e7989be777c26309ec2a7f40fd
Joel Reeves <jreeves@tonicdesign.com>
2015-12-30 14:52:07 -0500

    removing unnecessary code to update the UI since it's also called in onResume

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
@@ -57,8 +57,6 @@ public class CrimeListFragment extends Fragment {
         }
 
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        updateUI();
     }
 
     @Override
