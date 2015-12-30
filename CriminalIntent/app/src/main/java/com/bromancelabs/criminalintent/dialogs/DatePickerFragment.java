a6011366b009cb49d5e34f0e1b0ee159860dbc1b
Joel Reeves <jreeves@tonicdesign.com>
2015-12-30 11:53:29 -0500

    using datepicker as the dialog's view

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/dialogs/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/dialogs/DatePickerFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.bromancelabs.criminalintent.R;
 
@@ -13,7 +15,10 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
