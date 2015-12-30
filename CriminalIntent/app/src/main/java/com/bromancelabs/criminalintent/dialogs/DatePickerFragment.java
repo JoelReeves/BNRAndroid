fce98bc702201622b18354c3af21aa03ebb66ec5
Joel Reeves <jreeves@tonicdesign.com>
2015-12-30 11:31:23 -0500

    creating class to display an AlertDialog

---
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/dialogs/DatePickerFragment.java
@@ -0,0 +1,21 @@
+package com.bromancelabs.criminalintent.dialogs;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+import com.bromancelabs.criminalintent.R;
+
+public class DatePickerFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
