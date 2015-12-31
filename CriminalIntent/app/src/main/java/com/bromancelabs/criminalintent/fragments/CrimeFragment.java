b7352e5ce08d0934f472ee5134739f7ad18d1467
Joel Reeves <jreeves@tonicdesign.com>
2015-12-31 13:01:59 -0500

    inflating menu in CrimeFragment

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -49,6 +52,7 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
 
         UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
@@ -116,6 +120,24 @@ public class CrimeFragment extends Fragment {
                 mCrime.setDate(date);
                 updateDate();
                 break;
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_delete_crime:
+                
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
