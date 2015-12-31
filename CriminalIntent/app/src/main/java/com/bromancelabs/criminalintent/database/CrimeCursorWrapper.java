6eb460cbd84832222c822fcf26d73979fc5d6177
Joel Reeves <jreeves@tonicdesign.com>
2015-12-31 12:30:13 -0500

    method to pull out relevant columns from the database

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/database/CrimeCursorWrapper.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/database/CrimeCursorWrapper.java
@@ -3,9 +3,21 @@ package com.bromancelabs.criminalintent.database;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 
+import com.bromancelabs.criminalintent.database.CrimeDbSchema.CrimeTable;
+import com.bromancelabs.criminalintent.models.Crime;
+
 public class CrimeCursorWrapper extends CursorWrapper {
 
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        return null;
     }
 }
