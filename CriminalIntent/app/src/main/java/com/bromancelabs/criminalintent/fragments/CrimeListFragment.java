71de309ce2c526f75a0af92cce49888aa37e32b0
Joel Reeves <jreeves@tonicdesign.com>
2016-01-02 12:39:07 -0500

    changing method visbility so that it can be called from CrimeListActivity

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/fragments/CrimeListFragment.java
@@ -124,7 +124,7 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
-    private void updateUI() {
+    public void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
