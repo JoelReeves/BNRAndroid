722278f94217b176e9de5b8327a88a2ad8a44428
Joel Reeves <jreeves@tonicdesign.com>
2016-01-02 11:06:25 -0500

    added method that returns the ID of the layout that the activity will inflate.

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/SingleFragmentActivity.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.bromancelabs.criminalintent.activities;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +13,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
@@ -23,6 +24,11 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+    }
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
     }
 
     protected abstract Fragment createFragment();
