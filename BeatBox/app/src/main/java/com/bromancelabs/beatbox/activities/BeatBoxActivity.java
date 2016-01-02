125d6e5303524afa8b61e2dd5c73b91fda4c434a
Joel Reeves <jreeves@tonicdesign.com>
2016-01-02 15:22:13 -0500

    creating layouts for recyclerview and button

--- BeatBox/app/src/main/java/com/bromancelabs/beatbox/activities/BeatBoxActivity.java
+++ BeatBox/app/src/main/java/com/bromancelabs/beatbox/activities/BeatBoxActivity.java
@@ -10,6 +10,6 @@ public class BeatBoxActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
+        setContentView(R.layout.fragment_beat_box);
     }
 }
