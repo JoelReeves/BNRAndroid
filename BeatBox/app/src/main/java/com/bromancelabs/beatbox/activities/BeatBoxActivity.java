4113d3ddd40c9c5c5b130e52796ffc856e449d63
Joel Reeves <jreeves@tonicdesign.com>
2016-01-02 13:58:48 -0500

    initial commit

---
+++ BeatBox/app/src/main/java/com/bromancelabs/beatbox/activities/BeatBoxActivity.java
@@ -0,0 +1,15 @@
+package com.bromancelabs.beatbox.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bromancelabs.beatbox.R;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_box);
+    }
+}
