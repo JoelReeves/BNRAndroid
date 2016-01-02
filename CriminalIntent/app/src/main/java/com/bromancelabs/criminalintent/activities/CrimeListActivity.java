f3b2d672c3249c6d927872c5832d1f19d3f271ec
Joel Reeves <jreeves@tonicdesign.com>
2016-01-02 12:23:18 -0500

    making class implement the Callbacks interface

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeListActivity.java
@@ -3,9 +3,11 @@ package com.bromancelabs.criminalintent.activities;
 import android.support.v4.app.Fragment;
 
 import com.bromancelabs.criminalintent.R;
+import com.bromancelabs.criminalintent.fragments.CrimeFragment;
 import com.bromancelabs.criminalintent.fragments.CrimeListFragment;
+import com.bromancelabs.criminalintent.models.Crime;
 
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
 
     @Override
     protected Fragment createFragment() {
@@ -15,5 +17,10 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        
     }
 }
