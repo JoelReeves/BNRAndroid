5d69ca0b0753605b0cbbeeedd0d32bc8ee633321
Joel Reeves <jreeves@tonicdesign.com>
2016-01-02 16:24:13 -0500

    building list of sounds after they are loaded

--- BeatBox/app/src/main/java/com/bromancelabs/beatbox/utils/BeatBox.java
+++ BeatBox/app/src/main/java/com/bromancelabs/beatbox/utils/BeatBox.java
@@ -5,6 +5,8 @@ import android.content.res.AssetManager;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BeatBox {
     private static final String TAG = BeatBox.class.getSimpleName();
@@ -12,19 +14,31 @@ public class BeatBox {
 
     private AssetManager mAssets;
 
+    private List<Sound> mSounds = new ArrayList<>();
+
     public BeatBox(Context context) {
         mAssets = context.getAssets();
         loadSounds();
     }
 
     private void loadSounds() {
-        String[] soundNames;
+        String[] soundNames = new String[0];
         try {
             soundNames = mAssets.list(SOUNDS_FOLDER);
             Log.i(TAG, "Found " + soundNames.length + " sounds");
         } catch (IOException ioe) {
             Log.e(TAG, "Could not list assets", ioe);
         }
+
+        for (String filename : soundNames) {
+            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
     }
 }
 
