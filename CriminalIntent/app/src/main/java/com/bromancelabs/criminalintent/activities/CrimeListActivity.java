b4af0f1b2295684639123cb1a592843c94227013
Joel Reeves <jreeves@tonicdesign.com>
2016-01-02 11:32:31 -0500

    using alias resources to show different layouts for phone and tablets

--- CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/bromancelabs/criminalintent/activities/CrimeListActivity.java
@@ -2,6 +2,7 @@ package com.bromancelabs.criminalintent.activities;
 
 import android.support.v4.app.Fragment;
 
+import com.bromancelabs.criminalintent.R;
 import com.bromancelabs.criminalintent.fragments.CrimeListFragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
@@ -9,5 +10,10 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_masterdetail;
     }
 }
