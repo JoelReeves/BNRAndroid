ad2935dceac2296f610b9b181ff75408b1ad147e
Joel Reeves <jreeves@tonicdesign.com>
2016-01-03 13:46:55 -0500

    preserving fragment state on rotation

--- BeatBox/app/src/main/java/com/bromancelabs/beatbox/fragments/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/bromancelabs/beatbox/fragments/BeatBoxFragment.java
@@ -34,6 +34,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
 
         mBeatBox = new BeatBox(getActivity());
     }
