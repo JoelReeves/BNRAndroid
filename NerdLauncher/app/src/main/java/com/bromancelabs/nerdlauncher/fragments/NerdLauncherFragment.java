5de417aae90f2c7dfb140f76afbb71af47b0e0ef
Joel Reeves <jreeves@tonicdesign.com>
2016-01-04 15:32:06 -0500

    creating explicit intent to launch the activity in the list when clicked

--- NerdLauncher/app/src/main/java/com/bromancelabs/nerdlauncher/fragments/NerdLauncherFragment.java
+++ NerdLauncher/app/src/main/java/com/bromancelabs/nerdlauncher/fragments/NerdLauncherFragment.java
@@ -1,6 +1,7 @@
 package com.bromancelabs.nerdlauncher.fragments;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -104,13 +105,14 @@ public class NerdLauncherFragment extends Fragment {
         }
     }
 
-    private class ActivityHolder extends RecyclerView.ViewHolder {
+    private class ActivityHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ResolveInfo mResolveInfo;
         private TextView mNameTextView;
 
         public ActivityHolder(View itemView) {
             super(itemView);
             mNameTextView = (TextView) itemView;
+            mNameTextView.setOnClickListener(this);
         }
 
         public void bindActivity(ResolveInfo resolveInfo) {
@@ -118,6 +120,13 @@ public class NerdLauncherFragment extends Fragment {
             PackageManager pm = getActivity().getPackageManager();
             String appName = mResolveInfo.loadLabel(pm).toString();
             mNameTextView.setText(appName);
+        }
+
+        @Override
+        public void onClick(View v) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+            Intent i = new Intent(Intent.ACTION_MAIN).setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+            startActivity(i);
         }
     }
 }
