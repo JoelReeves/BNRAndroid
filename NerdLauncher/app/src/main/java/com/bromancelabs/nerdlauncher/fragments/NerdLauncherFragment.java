d4d80a72191ac88553c72c74b79011c624e71067
Joel Reeves <jreeves@tonicdesign.com>
2016-01-04 15:12:13 -0500

    adding method that tells how many apps on a device have a MAIN action and a LAUNCHER category

--- NerdLauncher/app/src/main/java/com/bromancelabs/nerdlauncher/fragments/NerdLauncherFragment.java
+++ NerdLauncher/app/src/main/java/com/bromancelabs/nerdlauncher/fragments/NerdLauncherFragment.java
@@ -1,21 +1,29 @@
 package com.bromancelabs.nerdlauncher.fragments;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bromancelabs.nerdlauncher.R;
 
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class NerdLauncherFragment extends Fragment {
     @Bind(R.id.rv_fragment_nerd_launcher) RecyclerView mRecyclerView;
+
+    private static final String TAG = NerdLauncherFragment.class.getSimpleName();
 
     public static NerdLauncherFragment newInstance() {
         return new NerdLauncherFragment();
@@ -33,6 +41,8 @@ public class NerdLauncherFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        setupAdapter();
+
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
@@ -40,5 +50,15 @@ public class NerdLauncherFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
+    }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
     }
 }
