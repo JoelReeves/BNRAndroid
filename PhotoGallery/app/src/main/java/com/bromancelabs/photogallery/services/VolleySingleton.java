453aa32d0a98c1c47c5e8e0da68603baa15a50ec
Joel Reeves <jreeves@tonicdesign.com>
2016-01-05 10:42:39 -0500

    helper class for performing networking requests using Google Volley

---
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/services/VolleySingleton.java
@@ -0,0 +1,60 @@
+package com.bromancelabs.photogallery.services;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class VolleySingleton {
+    private static VolleySingleton volleySingleton;
+    private static Context context;
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+
+    private VolleySingleton(Context context) {
+        VolleySingleton.context = context;
+
+        requestQueue = getRequestQueue();
+
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (volleySingleton == null) {
+            volleySingleton = new VolleySingleton(context);
+        }
+        return volleySingleton;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+}
