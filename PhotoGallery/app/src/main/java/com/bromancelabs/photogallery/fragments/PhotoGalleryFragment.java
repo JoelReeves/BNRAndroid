bc7fffd963173062085770b6788767540beee338
Joel Reeves <jreeves@tonicdesign.com>
2016-01-05 10:47:01 -0500

    using google volley to perform a string request to get data from the given URL

--- PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/fragments/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/fragments/PhotoGalleryFragment.java
@@ -5,17 +5,26 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.bromancelabs.photogallery.R;
+import com.bromancelabs.photogallery.services.VolleySingleton;
+import com.bromancelabs.photogallery.utils.NetworkUtils;
+import com.bromancelabs.photogallery.utils.SnackBarUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final int GRID_COLUMNS = 3;
+    private static final String TAG = PhotoGalleryFragment.class.getSimpleName();
+    private static final String URL = "https://www.google.com";
 
     @Bind(R.id.rv_photo_gallery) RecyclerView mPhotoRecyclerView;
 
@@ -42,11 +51,40 @@ public class PhotoGalleryFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), GRID_COLUMNS));
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!NetworkUtils.isNetworkAvailable(getActivity())) {
+            SnackBarUtils.showPlainSnackBar(getActivity(), R.string.snackbar_network_unavailable);
+        } else {
+            volleyStringRequest();
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
+    }
+
+    private void volleyStringRequest() {
+        StringRequest stringRequest = new StringRequest(URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Response: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.toString());
+                SnackBarUtils.showPlainSnackBar(getActivity(), R.string.snackbar_download_error);
+            }
+        });
+
+        VolleySingleton.getInstance(getActivity()).addToRequestQueue(stringRequest);
     }
 }
