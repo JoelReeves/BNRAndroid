90ae0eab22b5209aaf17cd93baab0bef4d1bad86
Joel Reeves <jreeves@tonicdesign.com>
2016-01-06 08:52:08 -0500

    renaming class

--- PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/models/PhotosObject.java
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/models/PhotosObject.java
@@ -3,14 +3,10 @@ package com.bromancelabs.photogallery.models;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class PhotosWrapper {
+public class PhotosObject {
     @SerializedName("photos")
     @Expose
     private Photos photos;
-
-    @SerializedName("stat")
-    @Expose
-    private String stat;
 
     public Photos getPhotos() {
         return photos;
@@ -18,13 +14,5 @@ public class PhotosWrapper {
 
     public void setPhotos(Photos photos) {
         this.photos = photos;
-    }
-
-    public String getStat() {
-        return stat;
-    }
-
-    public void setStat(String stat) {
-        this.stat = stat;
     }
 }
--- PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/services/FlickerPhotoService.java
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/services/FlickerPhotoService.java
@@ -1,6 +1,6 @@
 package com.bromancelabs.photogallery.services;
 
-import com.bromancelabs.photogallery.models.PhotosWrapper;
+import com.bromancelabs.photogallery.models.PhotosObject;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,5 +9,5 @@ import retrofit2.http.Query;
 public interface FlickerPhotoService {
 
     @GET("/services/rest/")
-    Call<PhotosWrapper> getRecentPhotos(@Query("method") String method, @Query("api_key") String apiKey, @Query("format") String format, @Query("nojsoncallback") String nojson, @Query("extras") String extras);
+    Call<PhotosObject> getRecentPhotos(@Query("method") String method, @Query("api_key") String apiKey, @Query("format") String format, @Query("nojsoncallback") String nojson, @Query("extras") String extras);
 }
