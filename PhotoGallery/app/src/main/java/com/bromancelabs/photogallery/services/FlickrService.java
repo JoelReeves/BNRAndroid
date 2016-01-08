d47c4fef46bd79a6953e859757ee0887e70b4fa9
Joel Reeves <jreeves@tonicdesign.com>
2016-01-08 14:02:55 -0500

    added new method to search for photos with the text query parameter

--- PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/services/FlickrService.java
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/services/FlickrService.java
@@ -10,4 +10,7 @@ public interface FlickrService {
 
     @GET("/services/rest/")
     Call<PhotosObject> getRecentPhotos(@Query("method") String method, @Query("api_key") String apiKey, @Query("format") String format, @Query("nojsoncallback") String nojson, @Query("extras") String extras);
+
+    @GET("/services/rest/")
+    Call<PhotosObject> searchPhotos(@Query("method") String method, @Query("api_key") String apiKey, @Query("format") String format, @Query("nojsoncallback") String nojson, @Query("text") String text, @Query("extras") String extras);
 }
