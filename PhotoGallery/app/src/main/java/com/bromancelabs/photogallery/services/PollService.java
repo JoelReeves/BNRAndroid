a51810b925ce56aebcbc1d6ca05d83916d1ae6c8
Joel Reeves <jreeves@tonicdesign.com>
2016-01-09 12:46:46 -0500

    method to create or cancel alarm

--- PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/services/PollService.java
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/services/PollService.java
@@ -1,8 +1,11 @@
 package com.bromancelabs.photogallery.services;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v4.content.LocalBroadcastManager;
 
 import com.bromancelabs.photogallery.fragments.PhotoGalleryFragment;
@@ -10,6 +13,7 @@ import com.bromancelabs.photogallery.utils.NetworkUtils;
 
 public class PollService extends IntentService {
     private static final String TAG = PollService.class.getSimpleName();
+    private static final long POLL_INTERVAL = 1000 * 60;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
@@ -33,5 +37,18 @@ public class PollService extends IntentService {
         Intent intent = new Intent(PhotoGalleryFragment.POLL_INTENT);
         intent.putExtra(PhotoGalleryFragment.POLL_KEY_ID, lastResultId);
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    public static void setServiceAlarm(Context context, boolean isOn) {
+        Intent intent = PollService.newIntent(context);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (isOn) {
+            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), POLL_INTERVAL, pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            pendingIntent.cancel();
+        }
     }
 }
