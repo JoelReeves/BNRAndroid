d4dabf27a714729a011dd4ea9578536a3e358961
Joel Reeves <jreeves@tonicdesign.com>
2016-01-10 15:35:54 -0500

    setting permission on BroadCastReceiver

--- PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/fragments/VisibleFragment.java
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/fragments/VisibleFragment.java
@@ -16,7 +16,7 @@ public abstract class VisibleFragment extends Fragment {
     public void onStart() {
         super.onStart();
         IntentFilter filter = new IntentFilter(PollService.ACTION_SHOW_NOTIFICATION);
-        getActivity().registerReceiver(mOnShowNotification, filter);
+        getActivity().registerReceiver(mOnShowNotification, filter, PollService.PRIVATE_PERMISSION, null);
     }
 
     @Override
