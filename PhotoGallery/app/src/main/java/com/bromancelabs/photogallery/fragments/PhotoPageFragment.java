6ee83974d938c9957e6f59e83c31afe3ac46fce8
Joel Reeves <jreeves@tonicdesign.com>
2016-01-11 09:47:08 -0500

    creating layout and fragment class to inflate a WebView

---
+++ PhotoGallery/app/src/main/java/com/bromancelabs/photogallery/fragments/PhotoPageFragment.java
@@ -0,0 +1,50 @@
+package com.bromancelabs.photogallery.fragments;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.bromancelabs.photogallery.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class PhotoPageFragment extends VisibleFragment {
+    private static final String ARG_URI = "photo_page_url";
+    private Uri mUri;
+
+    @Bind(R.id.wv_fragment_photo_page) WebView mWebView;
+
+    public static PhotoPageFragment newInstance(Uri uri) {
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_URI, uri);
+
+        PhotoPageFragment fragment = new PhotoPageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUri = getArguments().getParcelable(ARG_URI);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_photo_page, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
