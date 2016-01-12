2feac88f718254e9010b611ecd6f13e45236771d
Joel Reeves <jreeves@tonicdesign.com>
2016-01-11 21:10:27 -0500

    adding boxes to List on ACTION_DOWN and ACTION_MOVE

--- DragAndDraw/app/src/main/java/com/bromancelabs/draganddraw/views/BoxDrawingView.java
+++ DragAndDraw/app/src/main/java/com/bromancelabs/draganddraw/views/BoxDrawingView.java
@@ -7,8 +7,16 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.bromancelabs.draganddraw.models.Box;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxDrawingView extends View {
     private static final String TAG = BoxDrawingView.class.getSimpleName();
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
 
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -28,15 +36,24 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                // Reset drawing state
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
 
