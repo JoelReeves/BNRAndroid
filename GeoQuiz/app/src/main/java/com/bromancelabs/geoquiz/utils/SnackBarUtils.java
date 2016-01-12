891a901f6d5173da05b41dcbb1a35834b7f1a238
Joel Reeves <jreeves@tonicdesign.com>
2016-01-12 13:57:50 -0500

    refactoring

--- GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/utils/SnackBarUtils.java
+++ GeoQuiz/app/src/main/java/com/bromancelabs/geoquiz/utils/SnackBarUtils.java
@@ -9,9 +9,31 @@ import android.support.v4.content.ContextCompat;
 
 public final class SnackBarUtils {
 
-    public static void showSnackBar(@NonNull Activity activity, @StringRes int text, @ColorRes int textColor, @ColorRes int backgroundColor) {
-        final Snackbar snackbar = Snackbar.make(
-                activity.findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT);
+    private static Snackbar createSnackBar(@NonNull Activity activity, @StringRes int text) {
+        return Snackbar.make(activity.findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT);
+    }
+
+    private static Snackbar createSnackBar(@NonNull Activity activity, String text) {
+        return Snackbar.make(activity.findViewById(android.R.id.content), text, Snackbar.LENGTH_SHORT);
+    }
+
+    public static void showPlainSnackBar(@NonNull Activity activity, @StringRes int text) {
+        createSnackBar(activity, text).show();
+    }
+
+    public static void showPlainSnackBar(@NonNull Activity activity, String text) {
+        createSnackBar(activity, text).show();
+    }
+
+    public static void showStyledBar(@NonNull Activity activity, @StringRes int text, @ColorRes int textColor, @ColorRes int backgroundColor) {
+        Snackbar snackbar = createSnackBar(activity, text);
+        snackbar.setActionTextColor(ContextCompat.getColor(activity, textColor));
+        snackbar.getView().setBackgroundColor(ContextCompat.getColor(activity, backgroundColor));
+        snackbar.show();
+    }
+
+    public static void showStyledBar(@NonNull Activity activity, String text, @ColorRes int textColor, @ColorRes int backgroundColor) {
+        Snackbar snackbar = createSnackBar(activity, text);
         snackbar.setActionTextColor(ContextCompat.getColor(activity, textColor));
         snackbar.getView().setBackgroundColor(ContextCompat.getColor(activity, backgroundColor));
         snackbar.show();
