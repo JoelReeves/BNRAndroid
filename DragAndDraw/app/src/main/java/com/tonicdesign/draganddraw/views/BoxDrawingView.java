d12636d1a8054df34bdc7aad7249e99890327ae5
Joel Reeves <jreeves@tonicdesign.com>
2016-01-11 20:31:53 -0500

    creating custom view

---
+++ DragAndDraw/app/src/main/java/com/tonicdesign/draganddraw/views/BoxDrawingView.java
@@ -0,0 +1,18 @@
+package com.tonicdesign.draganddraw.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoxDrawingView extends View {
+
+    // Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
