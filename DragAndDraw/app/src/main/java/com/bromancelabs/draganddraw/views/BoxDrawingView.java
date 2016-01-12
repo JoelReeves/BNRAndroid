127a90dc28d8f1fdc7cf0790ebde1102a1dd6de3
Joel Reeves <jreeves@tonicdesign.com>
2016-01-11 20:51:30 -0500

    logging touch events

--- DragAndDraw/app/src/main/java/com/bromancelabs/draganddraw/views/BoxDrawingView.java
+++ DragAndDraw/app/src/main/java/com/bromancelabs/draganddraw/views/BoxDrawingView.java
@@ -1,10 +1,14 @@
 package com.bromancelabs.draganddraw.views;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BoxDrawingView extends View {
+    private static final String TAG = BoxDrawingView.class.getSimpleName();
 
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -14,5 +18,29 @@ public class BoxDrawingView extends View {
     // Used when inflating the view from XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+
+        Log.d(TAG, action + " at x=" + current.x + ", y=" + current.y);
+        return true;
     }
 }
