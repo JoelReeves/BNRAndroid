8c45144e5031dec2ecc9119c0cf9d04f9ee0fde3
Joel Reeves <jreeves@tonicdesign.com>
2016-01-13 20:34:14 -0500

    checking for Play Services when activity starts

--- Locatr/app/src/main/java/com/bromancelabs/locatr/activities/LocatrActivity.java
+++ Locatr/app/src/main/java/com/bromancelabs/locatr/activities/LocatrActivity.java
@@ -1,14 +1,45 @@
 package com.bromancelabs.locatr.activities;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 
-import com.bromancelabs.locatr.activities.SingleFragmentActivity;
 import com.bromancelabs.locatr.fragments.LocatrFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class LocatrActivity extends SingleFragmentActivity {
+    private static final int REQUEST_ERROR = 0;
+    private GoogleApiAvailability mGoogleInstance = GoogleApiAvailability.getInstance();
 
     @Override
     protected Fragment createFragment() {
         return LocatrFragment.newInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkGooglePlayServices();
+    }
+
+    private void checkGooglePlayServices() {
+        int resultCode = mGoogleInstance.isGooglePlayServicesAvailable(this);
+
+        if (ConnectionResult.SUCCESS != resultCode) {
+            if (mGoogleInstance.isUserResolvableError(resultCode)) {
+                Dialog errorDialog = mGoogleInstance.getErrorDialog(this, resultCode, REQUEST_ERROR);
+
+                if (null != errorDialog) {
+                    errorDialog.show();
+                    errorDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            finish();
+                        }
+                    });
+                }
+            }
+        }
     }
 }
