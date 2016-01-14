ac6a6e857a47afda9be416572ec571d9c1e26360
Joel Reeves <jreeves@tonicdesign.com>
2016-01-14 16:01:16 -0500

    getLocation method to obtain device's location when menu item is pressed

--- Locatr/app/src/main/java/com/bromancelabs/locatr/fragments/LocatrFragment.java
+++ Locatr/app/src/main/java/com/bromancelabs/locatr/fragments/LocatrFragment.java
@@ -1,14 +1,17 @@
 package com.bromancelabs.locatr.fragments;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -19,7 +22,10 @@ import com.bromancelabs.locatr.models.PhotosObject;
 import com.bromancelabs.locatr.services.FlickrService;
 import com.bromancelabs.locatr.services.RetrofitSingleton;
 import com.bromancelabs.locatr.utils.DialogUtils;
+import com.bromancelabs.locatr.utils.SnackBarUtils;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import java.util.List;
@@ -28,6 +34,9 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class LocatrFragment extends Fragment {
     private static final String TAG = LocatrFragment.class.getSimpleName();
@@ -41,8 +50,6 @@ public class LocatrFragment extends Fragment {
     @Bind(R.id.iv_image) ImageView mImageView;
 
     private GoogleApiClient mGoogleClient;
-
-    private FlickrService mFlickrService;
 
     private Dialog mProgressDialog;
 
@@ -100,13 +107,45 @@ public class LocatrFragment extends Fragment {
         menu.findItem(R.id.action_locate).setEnabled(mGoogleClient.isConnected());
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_locate:
+                getLocation();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void getLocation() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setNumUpdates(1);
+        locationRequest.setInterval(0);
+
+
+        if (ContextCompat.checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getActivity(), ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            SnackBarUtils.showPlainSnackBar(getActivity(), R.string.snackbar_location_permissions_error);
+        } else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleClient, locationRequest, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    Log.i(TAG, "Got a fix: " + location);
+                }
+            });
+        }
+    }
+
     private void getFlickrPhotos(Location location) {
         mProgressDialog = DialogUtils.showProgressDialog(getActivity());
-        mFlickrService = RetrofitSingleton.getInstance(URL).create(FlickrService.class);
+        FlickrService flickrService = RetrofitSingleton.getInstance(URL).create(FlickrService.class);
 
         String searchString = "dogs";
 
-        mFlickrService.searchPhotos(FLICKR_API_SEARCH_PHOTOS, FLICKR_API_KEY, FLICKR_API_FORMAT, FLICKR_API_JSON_CALLBACK, searchString, location.getLatitude(), location.getLongitude(), FLICKR_API_EXTRAS).enqueue(new Callback<PhotosObject>() {
+        flickrService.searchPhotos(FLICKR_API_SEARCH_PHOTOS, FLICKR_API_KEY, FLICKR_API_FORMAT, FLICKR_API_JSON_CALLBACK, searchString, location.getLatitude(), location.getLongitude(), FLICKR_API_EXTRAS).enqueue(new Callback<PhotosObject>() {
             @Override
             public void onResponse(Response<PhotosObject> response) {
                 if (response.isSuccess()) {
