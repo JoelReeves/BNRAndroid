1dbdddb8e2e777aba9a9897e372bb915d715b8b9
Joel Reeves <jreeves@tonicdesign.com>
2016-01-14 14:54:44 -0500

    method to search for Flickr photos at a given location

--- Locatr/app/src/main/java/com/bromancelabs/locatr/fragments/LocatrFragment.java
+++ Locatr/app/src/main/java/com/bromancelabs/locatr/fragments/LocatrFragment.java
@@ -1,8 +1,11 @@
 package com.bromancelabs.locatr.fragments;
 
+import android.app.Dialog;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,13 +14,23 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bromancelabs.locatr.R;
+import com.bromancelabs.locatr.models.Photo;
+import com.bromancelabs.locatr.models.PhotosObject;
+import com.bromancelabs.locatr.services.FlickrService;
+import com.bromancelabs.locatr.services.RetrofitSingleton;
+import com.bromancelabs.locatr.utils.DialogUtils;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LocatrFragment extends Fragment {
+    private static final String TAG = LocatrFragment.class.getSimpleName();
     private static final String URL = "https://api.flickr.com/services/rest/";
     private static final String FLICKR_API_KEY = "b71c3d2d57d035bf593c78dcb4b659d1";
     private static final String FLICKR_API_SEARCH_PHOTOS = "flickr.photos.search";
@@ -28,6 +41,12 @@ public class LocatrFragment extends Fragment {
     @Bind(R.id.iv_image) ImageView mImageView;
 
     private GoogleApiClient mGoogleClient;
+
+    private FlickrService mFlickrService;
+
+    private Dialog mProgressDialog;
+
+    private List<Photo> mPhotoList;
 
     public static LocatrFragment newInstance() {
         return new LocatrFragment();
@@ -79,6 +98,39 @@ public class LocatrFragment extends Fragment {
         inflater.inflate(R.menu.fragment_locatr, menu);
 
         menu.findItem(R.id.action_locate).setEnabled(mGoogleClient.isConnected());
+    }
+
+    private void getFlickrPhotos(Location location) {
+        mProgressDialog = DialogUtils.showProgressDialog(getActivity());
+        mFlickrService = RetrofitSingleton.getInstance(URL).create(FlickrService.class);
+
+        String searchString = "dogs";
+
+        mFlickrService.searchPhotos(FLICKR_API_SEARCH_PHOTOS, FLICKR_API_KEY, FLICKR_API_FORMAT, FLICKR_API_JSON_CALLBACK, searchString, location.getLatitude(), location.getLongitude(), FLICKR_API_EXTRAS).enqueue(new Callback<PhotosObject>() {
+            @Override
+            public void onResponse(Response<PhotosObject> response) {
+                if (response.isSuccess()) {
+                    final long responseSize = Long.parseLong(response.body().getPhotos().getTotal());
+                    Log.d(TAG, "JSON response # of photos: " + responseSize);
+                    mPhotoList = response.body().getPhotos().getPhoto();
+                } else {
+                    Log.e(TAG, "Error: " + response.message());
+                }
+                dismissDialog(mProgressDialog);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e(TAG, "Error: " + t.toString());
+                dismissDialog(mProgressDialog);
+            }
+        });
+    }
+
+    private void dismissDialog(Dialog dialog) {
+        if (dialog != null) {
+            dialog.dismiss();
+        }
     }
 
     private void buildGoogleApiClient() {
